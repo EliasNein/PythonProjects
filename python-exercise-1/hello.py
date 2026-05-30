4329ab76e42b0fbf475a0b9c030bd42f895f8a1a
Elias Nein <eliasnein09@gmail.com>
2026-05-30 23:09:10 +0200

    course-finished

---
+++ python-exercise-1/hello.py
@@ -0,0 +1,26 @@
+"""
+drivers_license = True
+drunk = False
+age = 18
+
+can_drive = age >= 18 and drivers_license and not drunk
+
+name = 'john'
+
+string = f'my name is {name}'
+
+temperature = 31
+if temperature > 30:
+	print("it's very hot!")
+elif temperature > 25:
+	print("it's hot!")
+else:
+	print("it's nice weather!")
+"""
+i = "hello"
+for i in range(5):
+	print(i)											
+
+# 1:48:00
+
+
